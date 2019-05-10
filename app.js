5e432bfef49612e9d3836605f9edba9709a0f902
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-05-10 10:42:01 +0000

    Added updated user event

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const { Kafka, logLevel } = require('kafkajs');
 const appUsers = [];
 const PORT = process.env.PORT || 5000;
 var isConsumerInitialized = false;
-var usersJson = {};
 var socketPtr;
 
 console.log("timestamp : " + (new Date()).getTime());
@@ -21,10 +20,10 @@ io.on('connection', (socket) => {
     socketPtr = socket;
 });
 
-setTimeout(() => {
+/*setTimeout(() => {
     console.log("Invoking socket emission");
     socketPtr.emit('myevent', "sample data using socket.io");
-}, 5000);
+}, 5000);*/
 
 
 io.on('disconnect', (socket) => {
@@ -37,6 +36,38 @@ function onRequest(request, response) {
   /* response.writeHead(200);
   response.write('Hello Noders');
   response.end(); */
+ 
+ var user_ip_address = ((request.headers['x-forwarded-for'] || '').split(',')[0] || request.connection.remoteAddress);
+ var user_agent = request.headers['user-agent'];
+ var uniqueGroupId = user_ip_address+"_"+user_agent+"_"+(new Date()).getTime();
+ 
+ if (!isConsumerInitialized) {
+    isConsumerInitialized = true;
+    console.log("topicConsumer is Initialized : ");
+    const consumer = kafka.consumer({ groupId: uniqueGroupId, fromBeginning: true  });
+
+    const run = async () => {
+      await consumer.connect();
+      await consumer.subscribe({ topic: userTopic, fromBeginning: true });
+      await consumer.run({
+        partitionsConsumedConcurrently: 5,
+        eachMessage: async ({ topic, partition, message }) => {
+            const msgRcvd = message.value.toString();
+          //console.log("msgRcvd : "+msgRcvd);
+          socketPtr.emit('appUserUpdate', msgRcvd);
+          if(!(msgRcvd in appUsers)){
+              appUsers.push(msgRcvd);
+              
+              //console.log(appUsers.length+" app users added so far");
+          }
+        },
+    
+      });
+    };
+    
+    run().catch(e => console.error(`[example/consumer] ${e.message}`, e));
+  }
+  
 
   if (request.url === "/") {
     fileSystem.readFile('./index.html', function (err, htmlContent) {
@@ -63,9 +94,4 @@ function onRequest(request, response) {
   } else {
     //TODO: 404 handling
   }
-}
-
-function getAvailableUsers() {
-    usersJson['users'] = appUsers;
-    return JSON.stringify(usersJson);//appUsers.join();
 }
