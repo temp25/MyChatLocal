ba233e02c8f3e834dcd7f6022b81750f9a3a7682
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-05-10 09:03:16 +0000

    Added socket io dependency

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fileSystem = require('fs');
+
 const { Kafka, logLevel } = require('kafkajs');
 const appUsers = [];
 const PORT = process.env.PORT || 5000;
@@ -8,104 +9,27 @@ var usersJson = {};
 
 console.log("timestamp : " + (new Date()).getTime());
 
-//const host = process.env.HOST_IP || ip.address()
+var server = http.createServer(onRequest);
+const io = require('socket.io')(server);
+server.listen(PORT); //bind to dynamic PORT set by heroku at runtime
+console.log('Server has started');
 
-const kafka = new Kafka({
-  logLevel: logLevel.INFO,
-  brokers: [
-    'velomobile-01.srvs.cloudkafka.com:9094',
-    'velomobile-02.srvs.cloudkafka.com:9094',
-    'velomobile-03.srvs.cloudkafka.com:9094'
-  ], //[`${host}:9094`],
-  clientId: 'chat-consumer',
-  ssl: {
-    rejectUnauthorized: true
-  },
-  sasl: {
-    mechanism: 'scram-sha-256',
-    username: '19uds2d2', //'test',
-    password: '48yZeR87btROThUIxvSzmooG4v7QZ3Pe', //'testtest',
-  },
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
 });
 
 
-const userTopic = "19uds2d2-testUsers2";
-const gId = "19uds2d2-consumer_"+(new Date()).getTime();
-
-console.log("\n topic : " + userTopic + "\n");
-console.log("\n groupId : " + gId + "\n");
-
-const errorTypes = ['unhandledRejection', 'uncaughtException']
-const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
-
-errorTypes.map(type => {
-  process.on(type, async e => {
-    try {
-      console.log(`process.on ${type}`)
-      console.error(e)
-      await consumer.disconnect()
-      process.exit(0)
-    } catch (_) {
-      process.exit(1)
-    }
-  })
-})
-
-signalTraps.map(type => {
-  process.once(type, async () => {
-    try {
-      await consumer.disconnect()
-    } finally {
-      process.kill(process.pid, type)
-    }
-  })
-})
+io.on('disconnect', (socket) => {
+    console.log('Client disconnected');
+});
 
 
-
-http.createServer(onRequest).listen(PORT); //bind to dynamic PORT set by heroku at runtime
-console.log('Server has started');
 
 function onRequest(request, response) {
   /* response.writeHead(200);
   response.write('Hello Noders');
   response.end(); */
- 
- var user_ip_address = ((request.headers['x-forwarded-for'] || '').split(',')[0] || request.connection.remoteAddress);
- var user_agent = request.headers['user-agent'];
- var uniqueGroupId = user_ip_address+"_"+user_agent+"_"+(new Date()).getTime();
- 
- //console.log("Request from user_ip_address : "+user_ip_address);
- //console.log("User Agent : "+user_agent);
- //console.log("uniqueGroupId : "+uniqueGroupId);
- 
-
-  //console.log('request url ' + request.url);
-
-  if (!isConsumerInitialized) {
-    isConsumerInitialized = true;
-    console.log("topicConsumer is Initialized : ");
-    const consumer = kafka.consumer({ groupId: uniqueGroupId, fromBeginning: true  });
-
-    const run = async () => {
-      await consumer.connect();
-      await consumer.subscribe({ topic: userTopic, fromBeginning: true });
-      await consumer.run({
-        partitionsConsumedConcurrently: 5,
-        eachMessage: async ({ topic, partition, message }) => {
-            const msgRcvd = message.value.toString();
-          console.log("msgRcvd : "+msgRcvd);
-          if(!(msgRcvd in appUsers)){
-              appUsers.push(msgRcvd);
-              console.log(appUsers.length+" app users added so far");
-          }
-        },
-    
-      });
-    };
-    
-    run().catch(e => console.error(`[example/consumer] ${e.message}`, e));
-  }
 
   if (request.url === "/") {
     fileSystem.readFile('./index.html', function (err, htmlContent) {
