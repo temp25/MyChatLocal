7d2487cf52ff78fbd9831ed2c9f99316781f524b
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-05-10 07:03:04 +0000

    Added code for users retrival from kafka queue logic

--- app_backup.js
+++ app_backup.js
@@ -65,26 +65,7 @@ signalTraps.map(type => {
 http.createServer(onRequest).listen(PORT); //bind to dynamic PORT set by heroku at runtime
 console.log('Server has started');
 
-function onRequest(request, response) {
-  /* response.writeHead(200);
-  response.write('Hello Noders');
-  response.end(); */
- 
- var user_ip_address = ((request.headers['x-forwarded-for'] || '').split(',')[0] || request.connection.remoteAddress);
- var user_agent = request.headers['user-agent'];
- var uniqueGroupId = user_ip_address+"_"+user_agent+"_"+(new Date()).getTime();
- 
- //console.log("Request from user_ip_address : "+user_ip_address);
- //console.log("User Agent : "+user_agent);
- //console.log("uniqueGroupId : "+uniqueGroupId);
- 
-
-  //console.log('request url ' + request.url);
-
-  if (!isConsumerInitialized) {
-    isConsumerInitialized = true;
-    console.log("topicConsumer is Initialized : ");
-    const consumer = kafka.consumer({ groupId: uniqueGroupId, fromBeginning: true  });
+const consumer = kafka.consumer({ groupId: '::1_Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0_1558424107367', fromBeginning: true  });
 
     const run = async () => {
       await consumer.connect();
@@ -104,17 +85,56 @@ function onRequest(request, response) {
     };
     
     run().catch(e => console.error(`[example/consumer] ${e.message}`, e));
+
+function onRequest(request, response) {
+  /* response.writeHead(200);
+  response.write('Hello Noders');
+  response.end(); */
+ 
+ var user_ip_address = ((request.headers['x-forwarded-for'] || '').split(',')[0] || request.connection.remoteAddress);
+ var user_agent = request.headers['user-agent'];
+ var uniqueGroupId = user_ip_address+"_"+user_agent+"_"+(new Date()).getTime();
+ 
+ //console.log("Request from user_ip_address : "+user_ip_address);
+ //console.log("User Agent : "+user_agent);
+ //console.log("uniqueGroupId : "+uniqueGroupId);
+ 
+
+  //console.log('request url ' + request.url);
+
+  if (!isConsumerInitialized) {
+    isConsumerInitialized = true;
+    /* const consumer = kafka.consumer({ groupId: uniqueGroupId, fromBeginning: true  });
+
+    const run = async () => {
+      await consumer.connect();
+      await consumer.subscribe({ topic: userTopic, fromBeginning: true });
+      await consumer.run({
+        partitionsConsumedConcurrently: 5,
+        eachMessage: async ({ topic, partition, message }) => {
+            const msgRcvd = message.value.toString();
+          console.log("msgRcvd : "+msgRcvd);
+          if(!(msgRcvd in appUsers)){
+              appUsers.push(msgRcvd);
+              console.log(appUsers.length+" app users added so far");
+          }
+        },
+    
+      });
+    };
+    
+    run().catch(e => console.error(`[example/consumer] ${e.message}`, e)); */
   }
 
-  if (request.url === "/") {
+  if (request.url == "/") {
     fileSystem.readFile('./index.html', function (err, htmlContent) {
       response.writeHead(200, {
         'Content-Type': 'text/html'
       });
-      response.write(String(htmlContent));
+      response.write(htmlContent + '');
       response.end();
     });
-  } else if (request.url === "/checkUser") {
+  } else if (request.url == "/checkUser") {
     console.log("Users check invoked");
     
     // sleep 5 seconds for final user updates to be received
@@ -122,11 +142,9 @@ function onRequest(request, response) {
         response.writeHead(200, {
             'Content-Type': 'text/plain'
         });
-        var _availUsers = getAvailableUsers();
-        console.log("_availUsers : "+_availUsers);
-        response.write(_availUsers);
+        response.write(getAvailableUsers());
         response.end();
-    }, 10000);
+    }, 5000);
     
   } else {
     //TODO: 404 handling
@@ -134,7 +152,5 @@ function onRequest(request, response) {
 }
 
 function getAvailableUsers() {
-    var userJso = {};
-    userJson['users'] = appUsers;
-  return JSON.stringify(userJson);//appUsers.join();
+  return appUsers.join();
 }
