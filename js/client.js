c53212056c474fd2560393f40b3125e5cb2fd395
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-05-11 10:48:10 +0530

    Modified file serving for imported js, css and image files

--- js/client.js
+++ js/client.js
@@ -1,12 +1,12 @@
 
-$(document).ready(function(){
-    
-    Swal.fire({
-        type: 'success',
-        title: "Document ready",
-        allowOutsideClick: () => false,
-        showConfirmButton: false,
-        timer: 2000, //dismiss after 2 seconds
-    });
+/* $(document).ready(function(){
 
+}); */    
+
+Swal.fire({
+    type: 'success',
+    title: "Document ready",
+    allowOutsideClick: () => false,
+    showConfirmButton: false,
+    timer: 2000, //dismiss after 2 seconds
 });
